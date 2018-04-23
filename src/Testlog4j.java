9d10596a6d2a474d078ea3e3eb91bc33b8b4539c
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-23 12:53:39 +0530

    Log4j

---
+++ src/Testlog4j.java
@@ -0,0 +1,29 @@
+import org.apache.log4j.Appender;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
+
+
+public class Testlog4j {
+
+	static Logger log= Logger.getLogger(Testlog4j.class.getName());
+	
+//	static Logger myLogger = Logger.getLogger(log4jTest.class.getName());
+
+
+
+	
+	
+	    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    	BasicConfigurator.configure();
+log.debug("this is debug log");
+
+log.info("this is info log");
+
+log.error("error in this proeject");	
+	}
+
+}
